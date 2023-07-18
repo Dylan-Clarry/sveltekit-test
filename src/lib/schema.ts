0564625ef8f66964c2d6374bf3ff44b06c90887b
Dylan <dylan.imail@gmail.com>
2023-07-18 10:05:03 -0400

    created table relations

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,8 +1,27 @@
-//import { relations } from "drizzle-orm";
-import { mysqlTable, varchar, boolean, serial } from "drizzle-orm/mysql-core";
+import { relations } from "drizzle-orm";
+import { mysqlTable, varchar, boolean, serial, int } from "drizzle-orm/mysql-core";
 
-export const todosTable = mysqlTable("todos", {
+export const tUser = mysqlTable("tUser", {
+    id: serial("id").primaryKey(),
+    username: varchar("username", { length: 25 }).notNull(),
+    email: varchar("email", { length: 25 }).notNull(),
+    password: varchar("password", { length: 255 }).notNull(),
+});
+
+export const tTodo = mysqlTable("tTodo", {
     id: serial("id").primaryKey(),
     text: varchar("text", { length: 25 }).notNull(),
-    completed: boolean("completed").notNull()
+    completed: boolean("completed").notNull(),
+    userid: int("userid").notNull(),
 });
+
+export const tUserRelations = relations(tUser, ({ many }) => ({
+    todos: many(tTodo),
+}));
+
+export const tTodoRelations = relations(tTodo, ({ one }) => ({
+    user: one(tUser, {
+        fields: [tTodo.userid],
+        references: [tUser.id],
+    }),
+}));
